3835d8c97bebad919c2df169f045f752f36b4a54
Roar12012 <omarc67272@gmail.com>
2025-09-14 15:55:14 -0500

    db updated

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,14 +1,14 @@
 import { PrismaClient } from '@prisma/client';
 // @ts-ignore
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs'; // <-- Cambiado de bcrypt a bcryptjs
 
 const prisma = new PrismaClient();
 
 async function main() {
   const users = [
-    { name: 'Usuario Free', email: 'free@test.com', password: await bcrypt.hash('123456', 10), role: 'FREE' },
-    { name: 'Usuario Basic', email: 'basic@test.com', password: await bcrypt.hash('123456', 10), role: 'BASIC' },
-    { name: 'Usuario Premium', email: 'premium@test.com', password: await bcrypt.hash('123456', 10), role: 'PREMIUM' },
+    { name: 'Usuario Basic', email: 'basic_user@test.com', password: await bcrypt.hash('123456', 10), role: 'BASIC' },
+    { name: 'Usuario Premium', email: 'premium_user@test.com', password: await bcrypt.hash('123456', 10), role: 'PREMIUM' },
+    { name: 'Usuario Pro', email: 'pro_user@test.com', password: await bcrypt.hash('123456', 10), role: 'PRO' },
   ];
 
   for (const user of users) {
